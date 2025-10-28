7d9fc8623dfbaa2df3e76760fb093839d0a1fcfc
Ben <meowcat@meowcat.site>
2025-10-28 19:15:31 +0000

    Did my first project!

---
+++ src/project1.java
@@ -0,0 +1,12 @@
+public class project1 {
+        public static void main(String[] args) {
+            String name = "Ben";
+            int age = 15;
+            double desiredSalary = 150.000;
+            char gender = 'o';
+            boolean lookingForJob = false;
+
+
+
+        }
+    }
