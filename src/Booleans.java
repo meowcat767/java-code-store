816da66c0fc5c1fc845967529adb057f6fbb2714
Ben <meowcat@meowcat.site>
2025-10-28 18:53:57 +0000

    add missin' semicolon

--- src/Booleans.java
+++ src/Booleans.java
@@ -1,6 +1,6 @@
 public class Booleans {
     public static void main(String[] args) {
         boolean intsCanHoldDecimals = false;
-        System.out.println(intsCanHoldDecimals)
+        System.out.println(intsCanHoldDecimals);
     }
 }
