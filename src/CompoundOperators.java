e06b210457e75aefcbd178b77093c17cdbb8f5c9
Ben <meowcat@meowcat.site>
2025-10-29 13:33:11 +0000

    Learned CompoundOperators

---
+++ src/CompoundOperators.java
@@ -0,0 +1,10 @@
+public class CompoundOperators {
+    public static void main(String[] args) {
+        int numCookies = 17;
+        numCookies /= 3;
+
+
+        // Add your code above
+        System.out.println(numCookies);
+    }
+}
