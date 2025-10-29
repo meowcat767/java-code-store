226aeb76a4f40925f8768a6d85b3479c1c8a1463
Ben <meowcat@meowcat.site>
2025-10-29 13:40:19 +0000

    greaterlessthan

---
+++ src/GreaterLessThan.java
@@ -0,0 +1,9 @@
+public class GreaterLessThan {
+    public static void main(String[] args) {
+        double creditsEarned = 176.5;
+        double creditsOfSeminar = 8;
+        double creditsToGraduate = 180;
+        boolean toGrad = creditsEarned > creditsToGraduate;
+        System.out.println(toGrad);
+    }
+}
